bd6a2f8a0221c814eb3a637e06d2f7082aa1e59e
juwel164 <juwelhossain16457@gmail.com>
2025-05-23 11:45:46 +0600

    addded icons to details task page

--- src/components/TaskDetails/TaskDetails.jsx
+++ src/components/TaskDetails/TaskDetails.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useLoaderData, useNavigate } from "react-router";
 import { formatDate } from "../FormateDate";
 import toast from "react-hot-toast";
+import { IoCaretBackSharp } from "react-icons/io5";
+import { FaHandshakeSimple } from "react-icons/fa6";
 
 const TaskDetails = () => {
   const task = useLoaderData();
@@ -37,21 +39,23 @@ const TaskDetails = () => {
     <div className="px-2 md:px-0">
       <div className="flex justify-center items-center my-5">
         <span className="font-semibold text-center mt-5 bg-purple-100 text-purple-700 px-4 border border-purple-700 mx-auto p-2 rounded-3xl">
-        You bid for {bids} opportunities.
-      </span>
+          You bid for {bids} opportunities.
+        </span>
       </div>
       <div className="max-w-4xl mx-auto p-6 md:px-12 my-5 bg-[#88dee6] shadow-md rounded-lg">
         <div className="flex justify-between items-center">
           <button
             onClick={() => navigate(-1)}
-            className=" px-4 py-2 bg-[#04284B] text-white hover:bg-[#222e39] text-sm rounded"
+            className=" px-4 py-2 bg-[#04284B] text-white hover:bg-[#222e39] text-sm rounded flex gap-1 items-center"
           >
+            <IoCaretBackSharp />
             Back
           </button>
           <button
             onClick={() => handleBidNowButton()}
-            className="px-4 py-2 bg-[#04284B] text-white hover:bg-[#222e39] text-sm rounded"
+            className="px-4 py-2 bg-[#04284B] text-white hover:bg-[#222e39] text-sm rounded flex gap-1 items-center"
           >
+            <FaHandshakeSimple />
             Bid Now
           </button>
         </div>
